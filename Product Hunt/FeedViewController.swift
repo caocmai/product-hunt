776a49d6d850e100cd6a0d222fee743c2becb69b
Cao Mai <caomai@rocketmail.com>
2020-04-17 19:02:37 -0700

    Decodable model

--- Product Hunt/FeedViewController.swift
+++ Product Hunt/FeedViewController.swift
@@ -10,13 +10,7 @@ import UIKit
 
 class FeedViewController: UIViewController {
     
-    var mockData: [Post] = {
-        var meTube = Post(id: 0, name: "MeTube", tagline: "Stream videos for free!", votesCount: 25, commentsCount: 4)
-        var boogle = Post(id: 1, name: "Boogle", tagline: "Search anything!", votesCount: 1000, commentsCount: 50)
-        var meTunes = Post(id: 2, name: "meTunes", tagline: "Listen to any song!", votesCount: 25000, commentsCount: 590)
-        
-        return [meTube, boogle, meTunes]
-    }()
+    var mockData: [Post] = []
     
     
     @IBOutlet weak var feedTableView: UITableView!
