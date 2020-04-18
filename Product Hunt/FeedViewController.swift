317e1b58b72bd391b9f9d40f10dc8f02390fc967
Cao Mai <caomai@rocketmail.com>
2020-04-17 18:45:34 -0700

    tests with mock data

--- Product Hunt/FeedViewController.swift
+++ Product Hunt/FeedViewController.swift
@@ -10,34 +10,53 @@ import UIKit
 
 class FeedViewController: UIViewController {
     
+    var mockData: [Post] = {
+        var meTube = Post(id: 0, name: "MeTube", tagline: "Stream videos for free!", votesCount: 25, commentsCount: 4)
+        var boogle = Post(id: 1, name: "Boogle", tagline: "Search anything!", votesCount: 1000, commentsCount: 50)
+        var meTunes = Post(id: 2, name: "meTunes", tagline: "Listen to any song!", votesCount: 25000, commentsCount: 590)
+        
+        return [meTube, boogle, meTunes]
+    }()
+    
     
     @IBOutlet weak var feedTableView: UITableView!
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         feedTableView.dataSource = self
         feedTableView.delegate = self
     }
-
-
+    
+    
 }
 
 // MARK: UITableViewDataSource
 extension FeedViewController: UITableViewDataSource {
-   /// Determines how many cells will be shown on the table view.
-   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       return 3
-   }
-
-   /// Creates and configures each cell.
-   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-       return UITableViewCell()
-  }
+    /// Determines how many cells will be shown on the table view.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return mockData.count
+    }
+    
+    /// Creates and configures each cell.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // dequeue and return an available cell, instead of creating a new cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "postCell", for: indexPath) as! PostTableViewCell
+        
+        let post = mockData[indexPath.row]
+        cell.post = post
+        
+        return cell
+    }
 }
 
 // MARK: UITableViewDelegate
 extension FeedViewController: UITableViewDelegate {
-  // Code to handle cell events goes here...
+    // Code to handle cell events goes here...
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        // provide a fixed size
+        return 250
+    }
 }
